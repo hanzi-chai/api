4fdebcd7defa36a691690c91e73059af4b034408
lost-melody <lost__melody@163.com>
2024-03-20 15:10:30 +0800

    fix incorrect user update route handler

--- src/router/users.ts
+++ src/router/users.ts
@@ -7,6 +7,6 @@ export const routerUsers = Router({ base: '/users' })
 	.get('/:id', users.Info)
 	.post('/', users.Create)
 	.delete('/:id', authorizedUser, authorizedAdmin, users.Delete)
-	.put('/:id', authorizedUser, users.Delete)
+	.put('/:id', authorizedUser, users.Update)
 	.put('/:id/promote', authorizedUser, authorizedSuper, users.Promote)
 	.put('/:id/disable', authorizedUser, authorizedSuper, users.Disable);
